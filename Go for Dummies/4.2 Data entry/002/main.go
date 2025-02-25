56dcce4bc793f57d5419858e18241ce712c29ef4
Sewadius <vertnather@gmail.com>
2025-02-25 15:53:26 +0300

    added "Go for Dummies"

---
+++ Go for Dummies/4.2 Data entry/002/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Поздравляю")
+	fmt.Println("ты почти")
+	fmt.Println("прошел")
+	fmt.Println("4 модуль")
+	fmt.Println("этого курса")
+}
---
+++ Go for Dummies/4.2 Data entry/003/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(
+		"0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+	)
+}
